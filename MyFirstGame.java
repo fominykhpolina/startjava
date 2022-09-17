6e8529c819bb36b4e6b5a92a0f4b26b35d14af8f
fominykhpolina <polina.fominykh4@gmail.com>
2022-09-17 21:06:15 +0300

    Final Version

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -1,18 +1,16 @@
 public class MyFirstGame {
 
     public static void main(String[] args) {
-        int computerNumber = 20;
-        int personNumber = 5;
-        while (computerNumber != personNumber) {
-            if (computerNumber < personNumber) {
-                System.out.println("Число " + personNumber + " больше того, что загадал компьютер");
-                personNumber--;
+        int num = 20;
+        int player = 5;
+        while (num != player) {
+            if (num < player) {
+                System.out.println("Число " + player + " больше того, что загадал компьютер");
+                player--;
+            } else {
+                System.out.println("Число " + player + " меньше того, что загадал компьютер");
+                player++;
             }
-            else {
-                System.out.println("Число " + personNumber + " меньше того, что загадал компьютер");
-                personNumber++;
-            }
-        } System.out.println("Вы победили! Загаданное число - " + computerNumber);
-
-        }
+        } System.out.println("Вы победили! Загаданное число - " + num);
     }
+}
