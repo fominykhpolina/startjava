a3b41666424a1e8ce5a169df62dd40d8b9f68f12
fominykhpolina <polina.fominykh4@gmail.com>
2022-10-03 21:46:36 +0300

    Final Version

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -1,74 +1,44 @@
 import java.util.Scanner;
+
 public class Calculator {
     private int num1;
     private int num2;
-    private String sign;
+    private char sign;
     private int result;
-    private String answer;
-
-    public int getNum1() {
-        return num1;
-    }
 
     public void setNum1(int num1) {
         this.num1 = num1;
     }
-
-    public int getNum2() {
-        return num2;
-    }
-
     public void setNum2(int num2) {
         this.num2 = num2;
     }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public void setSign(String sign) {
+    public void setSign(char sign) {
         this.sign = sign;
     }
-
     public int getResult() {
-        return result;
-    }
-
-    public void setResult(int result) {
-        this.result = result;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public void count() {
         switch (sign) {
-            case "+" :
+            case '+' :
                 result = num1 + num2;
                 break;
-            case "-" :
+            case '-' :
                 result = num1 - num2;
                 break;
-            case "*" :
+            case '*' :
                 result = num1 * num2;
                 break;
-            case "/" :
+            case '/' :
                 result = num1 / num2;
                 break;
-            case "%" :
+            case '%' :
                 result = num1 % num2;
                 break;
-            case "^" :
+            case '^' :
                 for(; num2 > 0; num2--) {
                     result *= num1;
                 }
                 break;
         }
+        return result;
     }
 }
 
--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
+
 public class CalculatorTest {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-
-        calculator.setAnswer("yes");
-       
-        while (calculator.getAnswer() == "yes") {
+        String answer = "yes";
+        while (answer.equals("yes")) {
             System.out.println("Введите первое число: ");
             calculator.setNum1(scanner.nextInt());
-
             System.out.println("Введите знак: ");
-            calculator.setSign(scanner.next());
-
+            calculator.setSign(scanner.next().charAt(0));
             System.out.println("Введите второе число: ");
             calculator.setNum2(scanner.nextInt());
-
-            calculator.count();
             System.out.println("Результат: " + calculator.getResult());
-
             System.out.println("Вы хотите продолжить вычисления? [yes/no]");
-            calculator.setAnswer(scanner.next());
-         }
+            answer = scanner.next();
+            while (!answer.equals("yes") && !answer.equals("no")) {
+                System.out.println("Неккоректный ввод");
+                System.out.println("Вы хотите продолжить вычисления? [yes/no]");
+                answer = scanner.next();
+            }
+            if (answer.equals("no")) {
+                    break;
+            }
+        }
     }
 }
