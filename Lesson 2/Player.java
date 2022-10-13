b4a400e8e37cdbef80b1bc95adeaa67f5c912a74
fominykhpolina <polina.fominykh4@gmail.com>
2022-10-13 21:11:30 +0300

    Final Version

---
+++ Lesson 2/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+
+    private String name;
+    private int number;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public int setNumber(int number) {
+        this.number = number;
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
